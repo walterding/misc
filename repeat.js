4302c5353d358387987af49b3696add5ee26af20
dinghui115046 <dinghui12345>
2019-03-07 11:19:40 +0800

    提交修改

---
+++ repeat.js
@@ -0,0 +1,23 @@
+// repeat print
+function print(c) {
+    return function () {
+        console.log(c)
+    }
+}
+
+let space=print('*')
+
+function repeat(f) {
+    return function () {
+        f()
+        return repeat(f)
+    }
+}
+
+function take(n,f) {
+    if(n==0)
+        return
+    take(n-1,f())
+}
+
+take(7,repeat(space))
